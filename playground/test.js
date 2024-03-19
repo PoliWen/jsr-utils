529031ed02239c130304fa3be4295fa69a229c94
文孝礼 <873619879@qq.com>
2024-03-19 08:43:08 +0800

    feat: jsr demo

--- playground/test.js
+++
@@ -1,4 +0,0 @@
-import { camelCase } from '@luca/cases'
-import {isString} from '@wen/utils'
-console.log(isString('hello')) // true
-console.log(camelCase('hello world')) // helloWorld
---
+++ playground/test.ts
@@ -0,0 +1,4 @@
+import { camelCase } from '@luca/cases'
+import { isString } from '@wen/utils'
+console.log(isString('hello'))
+console.log(camelCase('hello world'))
