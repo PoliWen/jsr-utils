793e85079744fc63bb5b4dae55a28bf50e6ac63c
文孝礼 <873619879@qq.com>
2024-03-21 01:04:13 +0800

    feat: 包含发布dist包

--- playground/test.ts
+++ playground/test.ts
@@ -1,6 +1,8 @@
 import { camelCase } from '@luca/cases'
-import { isString } from '@wen/utils'
+import { isString, myClone } from '@wen/utils'
 import { isArray } from 'poli-utils'
 console.log(isString('hello'))
 console.log(isArray([1,2,3]))
 console.log(camelCase('hello world'))
+const arr = [1,2,3]
+console.log(myClone()(arr))
